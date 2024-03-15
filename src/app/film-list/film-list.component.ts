12d4da3e652651a40900ba966f3c0b1150927773
msc <msc>
2024-03-15 15:25:41 -0400

    affichage films dans composant film-list grace aux service film

--- src/app/film-list/film-list.component.ts
+++ src/app/film-list/film-list.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { FilmService } from '../services/film.service'
+import { HttpClientModule } from '@angular/common/http'
+import { CommonModule } from '@angular/common'
+import { Film } from '../models/film.model'
+import { Subscription } from 'rxjs'
 
 @Component({
-  selector: 'app-film-list',
-  standalone: true,
-  imports: [],
-  templateUrl: './film-list.component.html',
-  styleUrl: './film-list.component.css'
+    selector: 'app-film-list',
+    standalone: true,
+    imports: [CommonModule, HttpClientModule],
+    providers: [FilmService],
+    templateUrl: './film-list.component.html',
+    styleUrl: './film-list.component.css',
 })
-export class FilmListComponent {
+export class FilmListComponent implements OnInit {
+    films: Film[] = []
 
+    filmsSub?: Subscription
+
+    constructor(private filmService: FilmService) {}
+
+    ngOnInit() {
+        this.filmsSub = this.filmService.filmsSubj.subscribe((films) => {
+            this.films = films
+        })
+        this.filmService.getFilms()
+    }
+
+    ngOnDestroy() {
+        this.filmsSub?.unsubscribe()
+    }
 }
