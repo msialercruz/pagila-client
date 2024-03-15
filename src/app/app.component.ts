9e4393397bc10c122698c49be644da01c68a8302
msc <msc>
2024-03-15 11:46:04 -0400

    ajout page d'acceuil et barre de navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core'
+import { RouterModule, RouterOutlet } from '@angular/router'
+import { HeaderComponent } from './header/header.component'
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    imports: [HeaderComponent, RouterModule, RouterOutlet],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'pagila-client';
+    title = 'pagila-client'
 }
