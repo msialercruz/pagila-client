59c1bec059759b796dd3045637ee12bb1655f0c5
msc <msc>
2024-03-15 11:57:46 -0400

    ajout routes 'root' et '/films' affichant les deux la liste des films

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
-import { Routes } from '@angular/router';
+import { Routes } from '@angular/router'
+import { FilmListComponent } from './film-list/film-list.component'
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'films', component: FilmListComponent },
+    { path: '', component: FilmListComponent },
+]
