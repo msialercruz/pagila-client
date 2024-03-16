0c4cb9e4aaf8df64039d73cb68e2fbf971c72008
msc <msc>
2024-03-15 20:30:59 -0400

    ajout pagination dans composant list-film

--- src/app/film-list/film-list.component.ts
+++ src/app/film-list/film-list.component.ts
@@ -4,11 +4,13 @@ import { HttpClientModule } from '@angular/common/http'
 import { CommonModule } from '@angular/common'
 import { Film } from '../models/film.model'
 import { Subscription } from 'rxjs'
+import { PaginationComponent } from '../pagination/pagination.component'
+import { ActivatedRoute, Router } from '@angular/router'
 
 @Component({
     selector: 'app-film-list',
     standalone: true,
-    imports: [CommonModule, HttpClientModule],
+    imports: [CommonModule, HttpClientModule, PaginationComponent],
     providers: [FilmService],
     templateUrl: './film-list.component.html',
     styleUrl: './film-list.component.css',
@@ -16,9 +18,18 @@ import { Subscription } from 'rxjs'
 export class FilmListComponent implements OnInit {
     films: Film[] = []
 
+    currentPage: number = 1
+
+    // TODO: update this with value returned from api when implemented
+    totalPages: number = 20
+
     filmsSub?: Subscription
 
-    constructor(private filmService: FilmService) {}
+    constructor(
+        private router: Router,
+        private route: ActivatedRoute,
+        private filmService: FilmService
+    ) {}
 
     ngOnInit() {
         this.filmsSub = this.filmService.filmsSubj.subscribe((films) => {
@@ -29,5 +40,16 @@ export class FilmListComponent implements OnInit {
 
     ngOnDestroy() {
         this.filmsSub?.unsubscribe()
+    }
+
+    // TODO: when accessing url from browser
+    // - display correct page
+    // - update pagination component (bidirectionnal)
+    loadFilms(page: number) {
+        this.filmService.getFilms(page)
+        this.router.navigate(['.'], {
+            relativeTo: this.route,
+            queryParams: { page },
+        })
     }
 }
