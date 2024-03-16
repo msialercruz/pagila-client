2c794006e963b7ef9a70d043f6116c3659fc6a48
msc <msc>
2024-03-15 20:30:14 -0400

    redirection route '/' vers '/films'

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,5 +3,5 @@ import { FilmListComponent } from './film-list/film-list.component'
 
 export const routes: Routes = [
     { path: 'films', component: FilmListComponent },
-    { path: '', component: FilmListComponent },
+    { path: '', redirectTo: '/films', pathMatch: 'full' },
 ]
