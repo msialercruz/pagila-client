ecfcf8b6026a539d556d87298dfd40e3f43b3a5a
msc <msc>
2024-06-05 16:00:05 -0400

    validation coté client (sans communiquer avec API) pour le formulaire d'ajout de film

--- src/app/film/add-film-form/add-film-form.component.ts
+++ src/app/film/add-film-form/add-film-form.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core'
-import { FormArray, FormControl, FormGroup } from '@angular/forms'
+import { FormArray, FormBuilder, FormControl, Validators } from '@angular/forms'
 import { ActivatedRoute } from '@angular/router'
 import { Observable, map, tap } from 'rxjs'
 import { Language } from '../../language/models/language.model'
+import { numberWithPrecisionAndScale } from './add-film-form-validators'
 
 @Component({
-    selector: 'app-add-film-form',
+    selector: 'add-film-form',
     templateUrl: './add-film-form.component.html',
     styleUrl: './add-film-form.component.css',
 })
@@ -17,7 +18,7 @@ export class AddFilmFormComponent implements OnInit {
     // observables
     languages$?: Observable<Language[]>
 
-    // control getters
+    // form controls getters
     get languageId() {
         return this.form.controls.languageId
     }
@@ -30,29 +31,49 @@ export class AddFilmFormComponent implements OnInit {
         return this.form.controls.specialFeatures
     }
 
-    // form group
-    form = new FormGroup({
-        title: new FormControl(),
-        description: new FormControl(),
-        releaseYear: new FormControl(new Date().getFullYear()),
-        languageId: new FormControl(),
-        originalLanguageId: new FormControl(),
-        rentalDuration: new FormControl(),
-        rentalRate: new FormControl(),
-        length: new FormControl(),
-        replacementCost: new FormControl(),
-        rating: new FormControl(this.defaultRating),
-        specialFeatures: new FormArray([] as FormControl<string | null>[]),
+    form = this.formBuilder.group({
+        title: ['', Validators.required],
+        description: ['', Validators.required],
+        releaseYear: [
+            new Date().getFullYear(),
+            Validators.compose([
+                Validators.required,
+                Validators.min(1901),
+                Validators.max(2155),
+            ]),
+        ],
+        languageId: [-1],
+        originalLanguageId: [-1],
+        rentalDuration: [
+            3,
+            Validators.compose([Validators.min(0), Validators.max(32767)]),
+        ],
+        rentalRate: [4.99, numberWithPrecisionAndScale(4, 2)],
+        length: [
+            0,
+            Validators.compose([
+                Validators.required,
+                Validators.min(0),
+                Validators.max(32767),
+            ]),
+        ],
+        replacementCost: [19.99, numberWithPrecisionAndScale(5, 2)],
+        rating: [this.defaultRating],
+        specialFeatures: new FormArray<FormControl<string>>([]),
     })
 
-    constructor(private route: ActivatedRoute) {}
+    constructor(
+        private route: ActivatedRoute,
+        private formBuilder: FormBuilder
+    ) {}
 
     ngOnInit() {
+        console.log(this.form.controls.releaseYear)
         this.languages$ = this.route.data.pipe(
             map(({ languages }) => languages),
             tap((languages) => {
-                this.languageId.setValue(languages[0].id)
-                this.originalLanguageId.setValue(languages[0].id)
+                this.languageId.setValue(languages[0].id!)
+                this.originalLanguageId.setValue(languages[0].id!)
             })
         )
     }
@@ -62,7 +83,11 @@ export class AddFilmFormComponent implements OnInit {
     onFeaturesChanged(specialFeatures: Set<string>) {
         this.specialFeatures.clear()
         specialFeatures.forEach((f) =>
-            this.specialFeatures.push(new FormControl(f))
+            this.specialFeatures.push(new FormControl(f, { nonNullable: true }))
         )
+    }
+
+    submitForm() {
+        console.log(this.form.value)
     }
 }
